225af1e8840003460d0c3fc3937c022708e7a5e3
artilugio0 <artilugio0@proton.me>
2026-04-10 15:10:25 -0300

    add flake with electron dependencies

---
+++ flake.nix
@@ -0,0 +1,79 @@
+{
+  description = "TS shell";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... } :
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          nodejs_24
+          typescript
+
+          libGL
+          glib
+          gtk3
+          nss
+          nspr
+          atk
+          at-spi2-atk
+          cups
+          dbus
+          libdrm
+          mesa  # for libGL
+          libx11
+          libxcomposite
+          libxdamage
+          libxext
+          libxfixes
+          libxrandr
+          libxcb
+          expat
+          fontconfig
+          freetype
+          libxkbcommon
+          udev
+          alsa-lib
+          cairo
+          pango
+          libgbm
+        ];
+
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [
+          libGL
+          glib
+          gtk3
+          nss
+          nspr
+          atk
+          at-spi2-atk
+          cups
+          dbus
+          libdrm
+          mesa  # for libGL
+          libx11
+          libxcomposite
+          libxdamage
+          libxext
+          libxfixes
+          libxrandr
+          libxcb
+          expat
+          fontconfig
+          freetype
+          libxkbcommon
+          udev
+          alsa-lib
+          cairo
+          pango
+          libgbm
+        ]);
+      };
+    };
+}
